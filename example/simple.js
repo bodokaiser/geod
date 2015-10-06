ef8ded3beca7a57cd1ae000d9acc5f57121eaaa1
Bodo Kaiser <i@bodokaiser.io>
2015-10-06 17:04:40 +0200

    added implementation details

--- example/simple.js
+++ example/simple.js
@@ -8,19 +8,18 @@ const height = 10
 const max = width * height * 0xff
 
 // will be used to weight distances between points
-var weigth = new Uint8Array(width * height).fill(1)
+var weight = new Uint8Array(width * height).fill(1)
 // will be used to write the transformed distances
 var distance = new Uint32Array(width * height).fill(max)
 
 // defines origin so shortest distance is calculated from here
-dist.set([0])
+distance.set([0])
 
 // builds a geodesic distance transform with our provided data
 geodes(10, 10)
-  .iters(100)
-  .weight(image)
+  .weight(weight)
   .distance(distance)
   .transform()
 
 // lets check out the distance
-console.log(distance)
+console.log(distance.toString())
--- test/transform.js
+++ test/transform.js
@@ -2,6 +2,133 @@ var assert = require('assert')
 
 var Transform = require('../lib/transform')
 
+const simple1 = new Uint8Array([
+  0, 1, 2,
+  1, 1, 2,
+  2, 2, 2
+])
+
+const simple2 = new Uint8Array([
+  2, 2, 2,
+  2, 1, 1,
+  2, 1, 0
+])
+
+const simple3 = new Uint8Array([
+  1, 1, 1,
+  1, 0, 1,
+  1, 1, 1
+])
+
+const complex = new Uint8Array([
+  0, 1, 2, 3, 4,
+  1, 1, 2, 3, 3,
+  2, 2, 2, 2, 2,
+  3, 3, 2, 1, 1,
+  4, 3, 2, 1, 0
+])
+
 describe('Transform', () => {
-  
+
+  beforeEach(() => {
+    this.t = new Transform(3, 3)
+    this.d = new Uint32Array(9).fill(1)
+    this.d.set([0])
+  })
+
+  describe('weight', () => {
+    it('should not throw error if ok', () => {
+      var w = new Uint32Array(9)
+
+      assert.throws(() => this.t.weigth())
+      assert.throws(() => this.t.weigth(w))
+    })
+    it('should throw error on invalid type', () => {
+      assert.throws(() => this.t.weigth(null))
+      assert.throws(() => this.t.weigth([1, 2]))
+    })
+    it('should throw error on invalid length', () => {
+      var s = new Uint32Array(8)
+      var l = new Uint32Array(10)
+
+      assert.throws(() => this.t.weigth(s))
+      assert.throws(() => this.t.weigth(l))
+    })
+  })
+
+  describe('distance', () => {
+    it('should not throw error if ok', () => {
+      this.t.distance(this.d)
+    })
+    it('should throw error on missing value', () => {
+      assert.throws(() => this.t.distance())
+    })
+    it('should throw error on invalid type', () => {
+      assert.throws(() => this.t.distance(null))
+      assert.throws(() => this.t.distance([1, 2]))
+    })
+    it('should throw error on invalid length', () => {
+      var s = new Uint32Array(8)
+      var l = new Uint32Array(10)
+
+      assert.throws(() => this.t.distance(s))
+      assert.throws(() => this.t.distance(l))
+    })
+    it('should throw error on missing unique values', () => {
+      var d = new Uint32Array(9)
+
+      assert.throws(() => this.t.distance(d))
+    })
+  })
+
+  describe('transform', () => {
+    it('should create empty weight', () => {
+      this.t.distance(this.d)
+      this.t.transform()
+
+      assert.ok(this.t._weight)
+    })
+    it('should throw error on missing distance', () => {
+      assert.throws(() => this.t.transform())
+    })
+    it('should transform simple1', () => {
+      this.d = new Uint8Array(9).fill(0xff)
+      this.d.set([0])
+
+      this.t.distance(this.d)
+      this.t.transform()
+
+      assert.deepEqual(this.d, simple1)
+    })
+    it('should transform simple2', () => {
+      this.d = new Uint8Array(9).fill(0xff)
+      this.d.set([0], 8)
+
+      this.t.distance(this.d)
+      this.t.transform()
+
+      assert.deepEqual(this.d, simple2)
+    })
+    it('should transform simple3', () => {
+      this.d = new Uint8Array(9).fill(0xff)
+      this.d.set([0], 4)
+
+      this.t.distance(this.d)
+      this.t.transform()
+
+      assert.deepEqual(this.d, simple3)
+    })
+    it('should transform complex', () => {
+      this.t = new Transform(5, 5)
+      this.d = new Uint8Array(25).fill(0xff)
+      this.d.set([0], 0)
+      this.d.set([0], 24)
+
+      this.t.distance(this.d)
+      this.t.transform()
+
+      assert.deepEqual(this.d, complex)
+    })
+  })
+
 })
